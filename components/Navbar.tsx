5ec0289e2b0978ceccdf36581dce2e2c82166486
Serm357 <kilangalilasalum@gmail.com>
2023-11-06 12:18:36 +0300

    modified logo image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
-        <Image src="/tzsafari.png" alt="logo" width={74} height={29} />
+        <Image src="/tzsafari.png" alt="logo" width={100} height={50} />
       </Link>
 
       <ul className="hidden h-full gap-12 lg:flex">
